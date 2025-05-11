1ae06461036056a3643a42a7edc009922e5490a2
nissim shukrun <nisssim1996@gmail.com>
2025-05-11 21:13:30 +0300

    connect to mongo db

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error to Connected");
+  });
+
+app.listen(5000, () => {
+  console.log(`Server is running on http://localhost:5000`);
+});
